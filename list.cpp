b32a2db9484ba29cefb96d1e02849ba5cc6cf3e1
Giovanna <giovvbatista@gmail.com>
2023-06-18 00:18:18 -0300

    added more methods

--- list.cpp
+++ list.cpp
@@ -2,177 +2,442 @@
 #include <sstream>
 #include <string>
 using namespace std;
+using size_type=unsigned long;
+
+template<typename T> 
+struct Node {
+    T data;
+    Node<T> *next;
+    Node<T> *prev;
+
+    Node(const T &d=T(), Node<T> *n=nullptr, Node<T> *p=nullptr) {
+        data=d;
+        next=n;
+        prev=p;
+    }
+};
 
 namespace sc {
-    using size_type=unsigned long;
-
     template<typename T> 
-    struct Node {
-        T data;
-        Node<T> *next;
-        Node<T> *prev;
+    class list;
+    template<typename T>
+    class MyFowardIterator;
+}
 
-        Node(const T &d=T(), Node<T> *n=nullptr, Node<T> *p=nullptr) {
-            data=d;
-            next=n;
-            prev=p;
-        }
-    };
-
-    template<typename T> 
-    class list {
-    //atributos
+template<typename T>
+class sc::MyFowardIterator {
     private:
-    Node<T> *head;
+    Node<T> *m_ptr;
 
     public:
     //aliases
-    using value_type=T;
-    using pointer=T*;
-    using reference=T&;
-    using const_reference=const T&;
+    typedef T value_type;
+    typedef T* pointer;
+    typedef T& reference;
+    typedef const T& const_reference;
 
     //construtores
+    MyFowardIterator(Node<T> *ptr=nullptr) { m_ptr=ptr; }
+    MyFowardIterator(const MyFowardIterator &other) { m_ptr=other.m_ptr; }
+
+    //sobrecarga de operadores
+    MyFowardIterator& operator=(const MyFowardIterator &other) { m_ptr=other.m_ptr; return *this; }
+    T& operator*() const { if(m_ptr != nullptr) return m_ptr->data; }
+    MyFowardIterator operator++() { m_ptr=m_ptr->next; return MyFowardIterator{m_ptr}; }
+    MyFowardIterator operator++(int) { MyFowardIterator dummy{*this}; m_ptr=m_ptr->next; return MyFowardIterator{dummy}; }
+    MyFowardIterator operator--() { m_ptr=m_ptr->prev; return MyFowardIterator{m_ptr}; }
+    MyFowardIterator operator--(int) { MyFowardIterator dummy{*this}; m_ptr=m_ptr->prev; return MyFowardIterator{dummy}; }
+    bool operator==(const MyFowardIterator &rhs) const { return m_ptr == rhs.m_ptr; }
+    bool operator!=(const MyFowardIterator &rhs) const { return m_ptr != rhs.m_ptr; }
+
+};
+
+template<typename T> 
+class sc::list {
+
+    //atributos
+    private:
+    Node<T> *m_head;
+    Node<T> *m_tail;
+    size_t m_len;
+
+    public:
+    //construtores
     //lista vazia
-    list() { head=new Node<T>(T(), nullptr, nullptr); }
-    //valores padrão
-    explicit list(size_type count) {
-        head=new Node<T>(T(), nullptr, nullptr);
-        Node<T> *p=head;
-        for(auto i=0; i<count; i++) {
-            Node<T> *nn=new Node<T>(i+1, nullptr, p);
-            p->next=nn;
-            p=nn;
-        }
+    list() { 
+        m_head=new Node<T>(T(), nullptr, nullptr); 
+        m_tail=new Node<T>(T(), nullptr, m_head); 
+        m_head->next=m_tail; 
+        m_len=0; 
     }
+
+    //valores padrão
+    explicit list(size_type count);
+
     //intervalo de ponteiros
     template<typename InputItr>
-    list(InputItr first, InputItr last) {
-        head=new Node<T>(T(), nullptr, nullptr);
-        Node<T> *p=head;
-        while(first != last) {
-            Node<T> *nn=new Node<T>(*first, nullptr, p);
-            p->next=nn;
-            p=nn;
-            first++;
-        }
-    }
+    list(InputItr first, InputItr last);
+
     //cópia de outra lista
-    list(const list& other) {
-        head=new Node<T>(T(), nullptr, nullptr);
-        Node<T> *runo=other.head->next;
-        Node<T> *p=head;
-        while (runo != nullptr) {
-            Node<T> *nn=new Node<T>(runo->data, nullptr, p);
-            p->next=nn;
-            p=nn;
-            runo=runo->next;
-        }
-    }
+    list(const list& other);
+
     //cópia de uma std::initializer_list
-    list(initializer_list<T> ilist) {
-        head=new Node<T>(T(), nullptr, nullptr);
-        Node<T> *p=head;
-        auto runner=ilist.begin();
-        while(runner != ilist.end()) {
-            Node<T> *nn=new Node<T>(*runner, nullptr, p);
-            p->next=nn;
-            p=nn;
-            runner++;
-        }
-    }
+    list(initializer_list<T> ilist);
+
     //destrutor
-    ~list() {
-        Node<T> *current=head;
-        while(current != nullptr) {
-            Node<T> *next=current->next;
-            delete current;
-            current=next;
-        }
-    }
+    ~list();
+
     //sobrecarga de operador de atribuição para uma outra lista
-    list& operator=(const list& other) {
-        if(this != &other) {
-            Node<T> *current=head;
-            while(current != nullptr) {
-                Node<T> *next=current->next;
-                delete current;
-                current=next;
-            }
-            head=new Node<T>(T(), nullptr, nullptr);
-            Node<T> *runo=other.head->next;
-            Node<T> *p=head;
-            while (runo != nullptr) {
-                Node<T> *nn=new Node<T>(runo->data, nullptr, p);
-                p->next=nn;
-                p=nn;
-                runo=runo->next;
-            }
-        }
-        return *this;
-    }
+    list& operator=(const list& other);
+
     //sobrecarga de operador de atribuição para uma std::initializer_list
-    list& operator=(initializer_list<T> ilist) {
-        Node<T> *current=head;
-        while(current != nullptr) {
-            Node<T> *next=current->next;
-            delete current;
-            current=next;
-        }    
-        head=new Node<T>(T(), nullptr, nullptr);
-        auto runner=ilist.begin();
-        Node<T> *p=head;
-        while (runner != ilist.end()) {
-            Node<T> *nn=new Node<T>(*runner, nullptr, p);
-            p->next=nn;
-            p=nn;
-            runner++;
-        }
-        return *this;
-    }
+    list& operator=(const initializer_list<T> ilist);
 
     //métodos de acesso
     //tamanho da lista
-    size_type size() const {
-        Node<T> *runner=head->next;
-        size_type count=0;
-        while(runner != nullptr) {
-            count++;
-            runner=runner->next;
-        }
-        return count;
-    }
+    size_type size() const { return m_len; }
+
     //retorna verdadeiro se a lista tiver vazia
-    bool empty() const { return head->next==nullptr; }
+    bool empty() const { return m_head->next==m_tail; }
+
     //retorna o primeiro valor da lista
-    const_reference front() const { if(!empty()) return head->next; }
+    T& front() { 
+        if(!empty()) 
+            return m_head->next->data; 
+        return T();
+    }
+
+    //retorna o último valor da lista
+    T& back() { 
+        if(!empty()) 
+            return m_tail->prev->data;
+        return T();
+    }
+
+    //retorna o último valor da lista de forma constante
+    const T& front() const { 
+        if(!empty()) 
+            return m_head->next->data; 
+        return T();
+    }
+
+    //retorna o último valor da lista de forma constante
+    const T& back() const { 
+        if(!empty()) 
+            return m_tail->prev->data;
+        return T();
+    }
+
+    //métodos de modificação
+    //apaga o conteúdo da lista
+    void clear();
+    
+    //adiciona um elemento no início da lista
+    void push_front(const T& value);
+
+    //adiciona um elemento no final da lista
+    void push_back(const T& value);
+
+    //remove o último elemento da lista
+    void pop_back();
+
+    //remove o primeiro elemento da lista
+    void pop_front();
+
+    //substitui o conteúdo da lista pelos elementos na range [first, last)
+    template <class InItr> 
+    void assign(InItr first, InItr last);
+
+    //substitui o conteúdo da lista pelos elementos da ilist
+    void assign(initializer_list<T> ilist) { assign(ilist.begin(), ilist.end()); }
+
+    //friends
+    //sobrecarga do ==
+    friend bool operator==(const list& lhs, const list& rhs) {
+        if(lhs.size() != rhs.size())
+            return false;
+        Node<T> *runl=lhs.m_head;
+        Node<T> *runr=rhs.m_head;
+        while(runl != lhs.m_tail) {
+            if(runl->data != runr->data)
+                return false;
+            runl=runl->next;
+            runr=runr->next;
+        }
+        return true;
+    }
+
+    //sobrecarga do !=
+    friend bool operator!=(const list& lhs, const list& rhs) { return !(lhs == rhs); }
+
     //imprimir os elementos em uma string
     string to_string() {
         ostringstream oss;
-        Node<T> *runner=head->next;
-        while(runner != nullptr) {
+        Node<T> *runner=m_head->next;
+        while(runner != m_tail) {
             oss<<"["<<runner->data<<"]->";
             runner=runner->next;
         }
         oss<<"<null>";
         return oss.str();
     }
-    };
-};
+
+    //métodos de acesso com iteradores
+    //retorna o primeiro nó da lista
+    MyFowardIterator<T> begin() { 
+        if(!empty()) 
+            return MyFowardIterator<T>(m_head->next); 
+        return MyFowardIterator<T>(); 
+    }
+
+    //retorna o último nó da lista
+    MyFowardIterator<T> end() { 
+        if(!empty()) 
+            return MyFowardIterator<T>(m_tail); 
+        return MyFowardIterator<T>(); 
+    }
+
+    //retorna o primeiro nó da lista de forma constante
+    const MyFowardIterator<T> cbegin() const { 
+        if(!empty()) 
+            return MyFowardIterator<T>(m_head->next); 
+        return MyFowardIterator<T>(); 
+    }
+
+    //retorna o último nó da lista de forma constante
+    const MyFowardIterator<T> cend() const { 
+        if(!empty()) 
+            return MyFowardIterator<T>(m_tail); 
+        return MyFowardIterator<T>(); 
+    }
+
+}; 
+
+template<typename T>
+sc::list<T>::list(size_type count) {
+    m_head=new Node<T>(T(), m_tail, nullptr);
+    m_tail=new Node<T>(T(), nullptr, m_head);
+    Node<T> *p=m_head;
+    for(auto i=0; i<count; i++) {
+        Node<T> *nn=new Node<T>(T(), m_tail, p);
+        p->next=nn;
+        m_tail->prev=nn;
+        p=nn;
+    }
+    m_len=count;
+}
+
+template<typename T>
+template<typename InputItr>
+sc::list<T>::list(InputItr first, InputItr last) {
+    m_head=new Node<T>(T(), m_tail, nullptr);
+    m_tail=new Node<T>(T(), nullptr, m_head);
+    Node<T> *p=m_head;
+    m_len=0;
+    while(first != last) {
+        Node<T> *nn=new Node<T>(*first, m_tail, p);
+        p->next=nn;
+        m_tail->prev=nn;
+        p=nn;
+        first++;
+        m_len++;
+    }
+}
+
+template<typename T>
+sc::list<T>::list(const list& other) {
+    m_head=new Node<T>(T(), m_tail, nullptr);
+    m_tail=new Node<T>(T(), nullptr, m_head);
+    Node<T> *runo=other.m_head->next;
+    Node<T> *p=m_head;
+    while (runo != other.m_tail) {
+        Node<T> *nn=new Node<T>(runo->data, m_tail, p);
+        p->next=nn;
+        m_tail->prev=nn;
+        p=nn;
+        runo=runo->next;
+    }
+    m_len=other.m_len;
+}
+
+template<typename T>
+sc::list<T>::list(initializer_list<T> ilist) {
+    m_head=new Node<T>(T(), m_tail, nullptr);
+    m_tail=new Node<T>(T(), nullptr, m_head);
+    Node<T> *p=m_head;
+    auto runner=ilist.begin();
+    m_len=0;
+    while(runner != ilist.end()) {
+        Node<T> *nn=new Node<T>(*runner, m_tail, p);
+        p->next=nn;
+        m_tail->prev=nn;
+        p=nn;
+        runner++;
+        m_len++;
+    }
+}
+
+template<typename T>
+sc::list<T>::~list() {
+    Node<T> *current=m_head;
+    while(current != nullptr) {
+        Node<T> *next=current->next;
+        delete current;
+        current=next;
+    }
+}
+
+template<typename T>
+sc::list<T>& sc::list<T>::operator=(const list<T>& other) {
+    if(this != &other) {
+        Node<T> *current=m_head;
+        while(current != nullptr) {
+            Node<T> *next=current->next;
+            delete current;
+            current=next;
+        }
+        m_head=new Node<T>(T(), m_tail, nullptr);
+        m_tail=new Node<T>(T(), nullptr, m_head);
+        Node<T> *runo=other.m_head->next;
+        Node<T> *p=m_head;
+        while (runo != other.m_tail) {
+            Node<T> *nn=new Node<T>(runo->data, m_tail, p);
+            p->next=nn;
+            m_tail->prev=nn;
+            p=nn;
+            runo=runo->next;
+        }
+    }
+    m_len=other.m_len;
+    return *this;
+}
+
+template<typename T>
+sc::list<T>& sc::list<T>::operator=(const initializer_list<T> ilist) {
+    Node<T> *current=m_head;
+    while(current != nullptr) {
+        Node<T> *next=current->next;
+        delete current;
+        current=next;
+    }    
+    m_head=new Node<T>(T(), m_tail, nullptr);
+    m_tail=new Node<T>(T(), nullptr, m_head);
+    auto runner=ilist.begin();
+    Node<T> *p=m_head;
+    m_len=0;
+    while (runner != ilist.end()) {
+        Node<T> *nn=new Node<T>(*runner, m_tail, p);
+        p->next=nn;
+        m_tail->prev=nn;
+        p=nn;
+        runner++;
+        m_len++;
+    }
+    return *this;
+}
+
+template<typename T>
+void sc::list<T>::clear() {
+    Node<T> *current=m_head->next;
+    while(current != m_tail) {
+        Node<T> *next=current->next;
+        delete current;
+        current=next;
+    }
+    m_head->next=m_tail;
+    m_tail->prev=m_head;
+    m_len=0;
+}
+
+template<typename T>
+void sc::list<T>::push_front(const T& value) {
+    Node<T> *nn=new Node<T>(value, nullptr, m_head);
+    if(empty()) {
+        m_head->next=nn;
+        nn->next=m_tail;
+        m_tail->prev=nn;
+        m_len++;
+        return;
+    }
+    nn->next=m_head->next;
+    m_head->next->prev=nn;
+    m_head->next=nn;
+    nn->prev=m_head;
+    m_len++;
+}
+
+template<typename T>
+void sc::list<T>::push_back(const T& value) {
+    Node<T> *nn=new Node<T>(value, nullptr, m_head);
+    Node<T> *prev=m_head;
+    if(empty()) {
+        m_head->next=nn;
+        nn->next=m_tail;
+        m_tail->prev=nn;
+        m_len++;
+        return;
+    }
+    while(prev->next != m_tail)
+        prev=prev->next;
+    nn->next=m_tail;
+    m_tail->prev=nn;
+    prev->next=nn;
+    nn->prev=prev;
+    m_len++;
+}
+
+template<typename T>
+void sc::list<T>::pop_back() {
+    if(empty())
+        return;
+    Node<T> *target=m_head;
+    while(target->next != m_tail)
+        target=target->next;
+    target->prev->next=m_tail;
+    m_tail->prev=target->prev;
+    delete target;
+    m_len--;
+}
+
+template<typename T>
+void sc::list<T>::pop_front() {
+    if(empty())
+        return;
+    Node<T> *target=m_head->next;
+    m_head->next=target->next;
+    target->next->prev=m_head;
+    delete target;
+    m_len--;
+}
+
+template<typename T>
+template <class InItr> 
+void sc::list<T>::assign(InItr first, InItr last) {
+    clear();
+    m_len=0;
+    while(first != last) {
+        push_back(*first);
+        first++;
+    }
+}
 
 int main() {
     //int A[]={9, 8, 7, 6, 5, 6, 4, 0};
     //auto n=sizeof(A)/sizeof(int);
-    sc::list<int> lista;
-    initializer_list<int> numbers={6, 6, 6, 6, 7, 8, 99, 0, 6};
-    sc::list<int> list(numbers);
+    initializer_list<int> numbers={-67, 6, 6, 0, 6, 6, 7, 8, 99, 0, 6, 100, 101};
+    sc::list<int> lista(numbers);
+    sc::list<int>list;
 
-    cout<<lista.size()<<endl;
-    if(lista.empty())
-        cout<<"tá vazio"<<endl;
-    lista=list;
-    cout<<lista.size();
-    if(lista.empty())
-        cout<<"tá vazio"<<endl;
+    list.assign(lista.begin(), lista.end());
 
+    sc::MyFowardIterator<int> i, j;
+    i=list.cbegin();
+
+    while(i != list.cend()) {
+        *i=*i+1;
+        cout<<*i<<" ";
+        ++i;
+    }
+
+    return 0;
 }
